f32e13f10abdd26e9bb4c3901b11e5f7a15a84e5
Omams21 <emmanuellaogun@gmail.com>
2022-02-15 10:44:46 +0100

    fix error from app.js

--- Desktop/property-pro-app/src/App.js
+++ Desktop/property-pro-app/src/App.js
@@ -1,11 +1,8 @@
 import {BrowserRouter as Router,  Route , Routes} from 'react-router-dom';
 import SignIn from './pages/Signin/Signin/Signin';
 import Signup from './pages/Signin/signup/Signup/Signup';
-
-
 import Homepage from './pages/Homepage/Homepage';
 
-import {BrowserRouter as Router,  Route , Routes} from 'react-router-dom';
 
 
 const App = ()=> {
